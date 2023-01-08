de5333276f05e01f3ca9ab3c52fc88a67d2bc4ce
AllenEtcetera <allencr1998@gmail.com>
2023-01-07 20:01:40 -0700

    Edited Program.cs file to complete Prep2 Homework (Grading System)

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What percentage grade do you have? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        string letter = "";
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You recieved {percent}% ({letter}) as your grade.");
+
+        if (percent >= 70)
+        {
+            Console.Write("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.Write("Unfortunately, you didn't pass. Please try again.");
+        }
     }
 }
