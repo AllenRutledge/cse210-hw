8345cae267b994b156bb5396a763a73734c01bcf
AllenEtcetera <allencr1998@gmail.com>
2023-01-09 14:27:55 -0700

    Completed Prep3 assignment (Random Number Guessing Game)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        bool guessed = false;
+        int turns = 0;
+        bool play_again = true;
+        while (play_again == true)
+        {
+            while(guessed == false)
+            {
+                Console.Write("What is your guess? ");
+                int guess = int.Parse(Console.ReadLine());
+                turns += 1;
+                if (guess > number)
+                {
+                    Console.WriteLine("Go lower.");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Go higher.");
+                }
+                else
+                {
+                    Console.WriteLine("You did it!");
+                    Console.WriteLine($"It took {turns} guesses!");
+                    guessed = true;
+                    Console.Write("Play again? y/n ");
+                    string yes_no = Console.ReadLine();
+                    if (yes_no.ToUpper() == "N")
+                    {
+                        play_again = false;
+                    }
+                }
+            }
+        }
     }
 }
