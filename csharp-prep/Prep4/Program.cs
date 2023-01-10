a9172b8eff379b228fdfb8f9c3b13371a69f3248
AllenEtcetera <allencr1998@gmail.com>
2023-01-09 22:33:10 -0700

    Completed Prep4 assignment (Number List Sort)

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,52 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Setting it up before the input loop.
+        List<int> numbers = new List<int>();
+        int userNumber = -42;
+
+        Console.WriteLine("Insert some numbers. (0 to quit)");
+        while (userNumber != 0)
+        {
+            Console.Write("Input Number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+
+        //Turns the sum into a float for compatibility.
+        float avg = ((float)sum) / numbers.Count;
+
+        int max = numbers[0];
+        foreach (int n in numbers)
+        {
+            if (n > max)
+            {
+                max = n;
+            }
+        }
+        int min = numbers[0];
+        foreach (int n in numbers)
+        {
+            if (n < min)
+            {
+                min = n;
+            }
+        }
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {avg}");
+        Console.WriteLine($"Highest: {max}");
+        Console.WriteLine($"Lowest: {min}");
     }
 }
